2111e6319fbbf56770a9178360a244d1ad4cb761
kasai <kasaipreston@gmail.com>
2022-02-08 14:22:03 -0800

    index model done

--- 02-Homework/Develop/models/index.js
+++ 02-Homework/Develop/models/index.js
@@ -1,37 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
-
-
-Reader.hasOne(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
-});
-
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
-
-
-LibraryCard.hasMany(Books, {
-  foreignKey: '_id',
-});
-
-Books.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
-
-
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "catagory_id",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: "catagory_id",
+});
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
+
 
 module.exports = {
   Product,
