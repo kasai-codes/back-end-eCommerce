8866b7e8bbcec9df57f26a86aa4bbbdb8308985a
kasai <kasaipreston@gmail.com>
2022-02-08 16:59:07 -0800

    SEQUELIZED MF!!

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -14,3 +15,8 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
